db262f354bb6771897417394120440590ae5c968
Moeein <moeeeinaali@gmail.com>
2026-03-07 14:50:43 +0330

    feat: add link and read repository implementations

---
+++ internal/repository/link_repository.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"context"
+	"url-shortener/internal/domain"
+
+	"gorm.io/gorm"
+)
+
+type LinkRepository struct {
+	db *gorm.DB
+}
+
+func NewLinkRepository(db *gorm.DB) *LinkRepository {
+	return &LinkRepository{db: db}
+}
+
+func (r *LinkRepository) Create(ctx context.Context, link *domain.Link) error {
+	return r.db.WithContext(ctx).Create(link).Error
+}
+
+func (r *LinkRepository) Disable(ctx context.Context, short string) error {
+	return r.db.
+		WithContext(ctx).
+		Model(&domain.Link{}).
+		Where("short_url = ?", short).
+		Update("disabled", true).
+		Error
+}
